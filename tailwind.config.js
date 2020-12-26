aab9deb6b9b1de020600c5757dc8f791b1a5a363
joshuarule <joshua@joshuarule.com>
2020-12-25 21:22:10 -0500

    styling setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -75,11 +75,11 @@ module.exports = {
       'white': 'var(--c-white)',
     },
     fontSize: {
-      'h1': ['1.75rem', '1'],
-      'h2': ['1.50rem', '1'],
-      'b1': ['1.25rem', '1'],
-      'b2': ['1.00rem', '1'],
-      'b3': ['0.75rem', '1'],
+      'h1': ['var(--f-h1--size)', 'var(--f-h1--lineHeight)'],
+      'h2': ['var(--f-h2--size)', 'var(--f-h2--lineHeight)'],
+      'h3': ['var(--f-h3--size)', 'var(--f-h3--lineHeight)'],
+      'b1': ['var(--f-b1--size)', 'var(--f-b1--lineHeight)'],
+      'b2': ['var(--f-b2--size)', 'var(--f-b2--lineHeight)'],
     }
   },
   variants: {},
