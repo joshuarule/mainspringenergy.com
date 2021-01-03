a9f754f8c7909229c652446f1012622c0954e315
Joshua Rule Dobson <joshua@joshuarule.com>
2021-01-03 13:57:57 -0500

    aspect ratio, ctaThird, image grid

--- src/components/cta.js
+++ src/components/cta.js
@@ -27,3 +27,33 @@ export default function Cta({
     </section>
   )
 }
+
+export const CtaThird = ({
+  className = '',
+  reverse = false,
+  title,
+  imgSrc,
+  imgAlt,
+  description,
+  linkUrl,
+  linkLabel = 'Learn more',
+  ...rest
+}) => (
+  <div className={`${className} grid lg:grid-cols-12 mb-g`} {...rest}>
+    <div className={`border-t-6 pt-c border-brand lg:col-span-4`}>
+      <h1 className="f-h3 mb-e">{title}</h1>
+      <p className="f-b1 mb-e">{description}</p>
+      <LinkPrimary className="f-b1 font-bold" to={linkUrl}>
+        {linkLabel}
+      </LinkPrimary>
+    </div>
+
+    <div
+      className={`md-max:mb-b relative aspect-w-16 aspect-h-9 lg:col-span-8 ${
+        reverse ? 'lg:order-first' : ''
+      }`}
+    >
+      <img src={imgSrc} alt={imgAlt} className="object-cover w-full h-full" />
+    </div>
+  </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Cta from '../components/cta'
+import Cta, { CtaThird } from '../components/cta'
 
 import Layout from '../components/layout'
 import { LinkPrimary } from '../components/link'
@@ -39,25 +39,82 @@ const IndexPage = () => (
       linkLabel="Read more"
       description="Fuel-flexible, and resilient power, saving carbon and cost today and accelerating the transition to the electric grid of the future."
     />
+
+    {/* Image Grid Component */}
     <section className="container mb-g">
       <h1 className="f-h2">Solutions</h1>
-      <div className="grid grid-cols-3 gap-1">
-        <div className="relative">
-          <div className="bg-gradient-to-t from-black to-transparent absolute fill left-0 right-0 bottom-0 top-0 opacity-40"></div>
-          <h3 className="absolute bottom-0 left-0 p-c text-white">Title</h3>
-          <img src="https://placehold.it/400x228" alt="" />
+      <div className="lg:grid lg:grid-cols-3 gap-1">
+        <div className="md-max:mb-b relative aspect-w-16 aspect-h-9">
+          <img
+            src="https://placehold.it/400x228"
+            alt=""
+            className="object-cover w-full h-full"
+          />
+          <div className="bg-gradient-to-t from-black to-transparent absolute fill left-0 right-0 bottom-0 top-0 opacity-40">
+            <h3 className="absolute bottom-0 left-0 p-c text-white">Title</h3>
+          </div>
         </div>
-        <div className="relative">
-          <div className="bg-gradient-to-t from-black to-transparent absolute fill left-0 right-0 bottom-0 top-0 opacity-40"></div>
-          <h3 className="absolute bottom-0 left-0 p-c text-white">Title</h3>
-          <img src="https://placehold.it/400x228" alt="" />
+        <div className="md-max:mb-b relative aspect-w-16 aspect-h-9">
+          <img
+            src="https://placehold.it/400x228"
+            alt=""
+            className="object-cover w-full h-full"
+          />
+          <div className="bg-gradient-to-t from-black to-transparent absolute fill left-0 right-0 bottom-0 top-0 opacity-40">
+            <h3 className="absolute bottom-0 left-0 p-c text-white">Title</h3>
+          </div>
         </div>
-        <div className="relative">
-          <div className="bg-gradient-to-t from-black to-transparent absolute fill left-0 right-0 bottom-0 top-0 opacity-40"></div>
-          <h3 className="absolute bottom-0 left-0 p-c text-white">Title</h3>
-          <img src="https://placehold.it/400x228" alt="" />
+        <div className="md-max:mb-b relative aspect-w-16 aspect-h-9">
+          <img
+            src="https://placehold.it/400x228"
+            alt=""
+            className="object-cover w-full h-full"
+          />
+          <div className="bg-gradient-to-t from-black to-transparent absolute fill left-0 right-0 bottom-0 top-0 opacity-40">
+            <h3 className="absolute bottom-0 left-0 p-c text-white">Title</h3>
+          </div>
         </div>
       </div>
+    </section>
+    <div className="container mb-g">
+      <figure className="border-t-6 border-smoke text-seaGreen pt-c lg:grid lg:grid-cols-2">
+        <div>
+          <blockquote
+            className="mb-e"
+            cite="https://www.huxley.net/bnw/four.html"
+          >
+            <p className="f-h3">
+              Words can be like X-rays, if you use them properly they’ll go
+              through anything. You read and you’re pierced.
+            </p>
+          </blockquote>
+          <figcaption className="f-b1">
+            Aldous Huxley, CEO Mainspring
+          </figcaption>
+        </div>
+      </figure>
+    </div>
+    <section className="container mb-g">
+      <CtaThird
+        title="Linear Generator Technology Designed From First Principles"
+        description="A fundamentally new power generation technology designed for high
+        efficiency, low cost, and ultimate flexibility."
+        linkUrl="/"
+        linkLabel="Read all about it"
+        imgSrc="https://placehold.it/400x228"
+        imgAlt="Alt text of the image"
+      />
+    </section>
+    <section className="container mb-g">
+      <CtaThird
+        title="Working at Mainspring"
+        description="Mainspring is solving essential problems every day with a diverse, talented team of individuals who share our vision to address the growing threat of climate change and the need for affordable electricity for all."
+        linkUrl="/jobs"
+        reverse
+        linkLabel="Opportunies"
+        imgSrc="https://placehold.it/400x228"
+        imgAlt="Alt text of the image"
+      />
     </section>
   </Layout>
 )
