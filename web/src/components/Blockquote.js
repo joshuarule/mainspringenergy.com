b3f165b00c93283f791ff3df2cec5deec067bb25
Joshua Rule Dobson <joshua@joshuarule.com>
2021-01-19 11:16:42 -0500

    vision blockuote style

--- web/src/components/Blockquote.js
+++ web/src/components/Blockquote.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Blockquote({ cite, text, author, options }) {
   if (!text) return null
   return options && options.offset ? (
-    <div className="blockquote lg:w-edge lg:h-0">
+    <div className="blockquote blockquote--offset lg:w-edge lg:h-0">
       <div className="lg:container lg:grid lg:grid-cols-12">
         <figure className="lg:col-span-3 border-t-6 border-smoke text-seaGreen pt-c mb-g">
           <div className="w-full">
