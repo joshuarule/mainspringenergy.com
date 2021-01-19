0a575804200be9e69298f1fd17fc3273426d23c8
Joshua Rule Dobson <joshua@joshuarule.com>
2021-01-19 13:58:53 -0500

    blockquote layout change

--- web/src/components/Blockquote.js
+++ web/src/components/Blockquote.js
@@ -16,8 +16,8 @@ export default function Blockquote({ cite, text, author, options }) {
       </div>
     </div>
   ) : (
-    <figure className="blockquote border-t-6 border-smoke text-seaGreen pt-c lg:grid lg:grid-cols-2 my-f">
-      <div>
+    <figure className="blockquote border-t-6 border-smoke text-seaGreen pt-c lg:grid lg:grid-cols-3 my-f">
+      <div className="col-span-2">
         <blockquote className="mb-e" cite={cite}>
           <p className="f-h3">{text}</p>
         </blockquote>
