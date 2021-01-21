d4d080b72e1b8f1008ff1297e3d7ac00b7f8b71e
Joshua Rule Dobson <joshua@joshuarule.com>
2021-01-21 13:37:57 -0500

    styles downloads

--- web/src/pages/solutions.js
+++ web/src/pages/solutions.js
@@ -103,6 +103,13 @@ export default ({ location }) => {
                 }
               }
             }
+            src {
+              src {
+                asset {
+                  url
+                }
+              }
+            }
           }
         }
         solutions_form {
@@ -158,8 +165,8 @@ export default ({ location }) => {
       <section className="bg-tan py-e">
         <div className="container">
           <div className="mb-f">
-            <h1 className="f-h2">{solutions_specs.title}</h1>
-            <p className="f-b1">{solutions_specs.subtitle}</p>
+            <h1 className="f-h2 text-seaGreen">{solutions_specs.title}</h1>
+            <p className="f-b1 text-seaGreen">{solutions_specs.subtitle}</p>
           </div>
           <div className="grid grid-cols-3 mb-g text-seaGreen items-center">
             <div>
@@ -196,22 +203,39 @@ export default ({ location }) => {
       </section>
       {/* specs_downloads */}
       <div className="container">
-        <h1>{solutions_downloads.title}</h1>
-        <ul>
+        <SectionTitle title={solutions_downloads.title} className="mb-f" />
+        <ul className="lg:grid lg:grid-cols-3">
           {solutions_downloads.items.map(item => (
-            <li>
-              <h1>{item.title}</h1>
-              <Img fluid={item.image.src.asset.fluid} alt={item.image.alt} />
-              <p>{item.subtitle}</p>
-              <p>{item.description}</p>
+            <li className="mb-g">
+              <div className="lg:grid lg:grid-cols-4">
+                <div className="lg:col-span-3 mb-d">
+                  <Img
+                    fluid={item.image.src.asset.fluid}
+                    alt={item.image.alt}
+                  />
+                </div>
+              </div>
+
+              <div className="col-span-4 col-start-1 lg:pr-e">
+                <h4 className="f-b1 font-bold">{item.title}</h4>
+                <p className="mb-d f-b1">{item.subtitle}</p>
+                <p className="f-b1 text-steel mb-d">{item.description}</p>
+                <LinkPrimary
+                  to={item.src.src.asset.url}
+                  className="f-b1 text-freshBlue"
+                  target="_blank"
+                >
+                  Download
+                </LinkPrimary>
+              </div>
             </li>
           ))}
         </ul>
       </div>
       {/* solutions_form */}
-      <div className="container">
+      {/* <div className="container">
         <SectionTitle {...solutions_form.title} className="mt-0 mb-e" />
-      </div>
+      </div> */}
     </Layout>
   )
 }
