42d1f22622006feb2c14b785fce6e545eb52f262
Joshua Rule Dobson <joshua@joshuarule.com>
2021-02-24 10:43:01 -0500

    fix for infoBar

--- web/src/components/Layout.js
+++ web/src/components/Layout.js
@@ -41,7 +41,7 @@ const Layout = ({ children, className = null, navTheme, location }) => {
                 }`}
               >
                 <div className="container flex-1 font-regular my-c f-b1">
-                  <span className="mr-c f-b1">{infoBar.title}</span>
+                  <span className="f-b1 mr-c">{infoBar.title}</span>
                   {hasLink && (
                     <>
                       <ParseLink
@@ -50,7 +50,7 @@ const Layout = ({ children, className = null, navTheme, location }) => {
                       >
                         <span className="sr-only">{infoBar.link.label}</span>
                       </ParseLink>
-                      <span className="text-white font-bold whitespace-nowrap">
+                      <span className="text-white font-bold whitespace-nowrap inline-block">
                         {infoBar.link.label}
                         <Icon
                           name="chevron"
